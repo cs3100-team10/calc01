c6ebb2462597b6cdb28a89386a95b522abdd2919
Ryan Duchene <rcdvxd@mst.edu>
2018-03-05 13:03:34 -0600

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,51 +1,100 @@
 #!/bin/bash
 
 # Get the current directory
-dev_dir=`pwd`
+dev_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 # Get a separate directory to build in.
 # By default, builds in "~/SDRIVE/cs3100/calc01-build"
 # Can be reconfigured with an environment variable CALC01_BUILD.
 build_dir=${CALC01_BUILD:-"$HOME/SDRIVE/cs3100/calc01-build"}
 
-# Has the build directory already been created?
-if [[ -d "$build_dir" ]]; then
-  # Yes, so just announce it
-  echo "calc01-build: building in $build_dir"
+## SETUP
+#  Ensures that there is a build directory.
+function setup()
+{
+  # Has the build directory already been created?
+  if [[ -d "$build_dir" ]]; then
+    # Yes, so just announce it
+    echo "setup: building in $build_dir"
 
-else
-  # No, so create it
-  echo "calc01-build: making build directory at $build_dir"
-  mkdir -p "$build_dir"
+  else
+    # No, so create it
+    echo "setup: making build directory at $build_dir"
+    mkdir -p "$build_dir"
 
-fi
+  fi
 
-# First make sure the build dir is clean
-echo "calc01-build: cleaning"
-rm -rf $build_dir/*
+}
 
-# Copy over the current directory
-echo "calc01-build: copying"
-cp -r $dev_dir/* $build_dir
+## CLEAN
+#  Cleans the build directory.
+function clean()
+{
 
-# Move to build directory
-cd "$build_dir"
+  # prep
+  setup
 
-# Build
-echo "calc01-build: qmake"
-qmake
+  # First make sure the build dir is clean
+  echo "clean: cleaning"
+  rm -rf $build_dir/*
 
-sleep 1 # otherwise we might invent time travel
+}
 
-echo "calc01-build: make all"
-make all
+## BUILD
+#  The bulk of the build logic. Most of the other tasks
+function build()
+{
 
-# Run the calculator
-# Note that when the user does this, the script will pause at this point
-# until the user closes the calculator
-# TODO: Allow the user to just attempt to compile and not run
-echo "calc01-build: ./T10Calculator"
-./T10Calculator
+  # prep
+  clean
 
-# Return to dev directory
-cd "$dev_dir"
+  # Copy over the current directory
+  echo "build: copying"
+  cp -r $dev_dir/* $build_dir
+
+  # Move to build directory
+  cd "$build_dir"
+
+  # Build
+  echo "build: qmake"
+  qmake
+
+  sleep 1 # otherwise we might invent time travel
+
+  echo "build: make all"
+  make all
+
+  cd "$dev_dir"
+
+}
+
+## RUN
+#  Run the project.
+function run()
+{
+
+  # Prep
+  build
+
+  # Run
+  echo "run: $build_dir/T10Calculator"
+  $build_dir/T10Calculator
+}
+
+case "$1" in
+  clean)
+    clean
+    ;;
+  
+  build)
+    build
+    ;;
+  
+  run)
+    run
+    ;;
+  
+  *)
+    build
+    ;;
+esac
