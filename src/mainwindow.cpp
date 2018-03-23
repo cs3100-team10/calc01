947337d939c305c05164fccc58202e0fc671b0a2
Sarah Schmitt <ses8cc@mst.edu>
2018-03-23 09:17:06 -0500

    memory works for keyboard input of basic calc

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -162,25 +162,24 @@ void MainWindow::on_lineEdit_returnPressed() {
     string toBeParsed = ui->lineEdit->text().toStdString();
 
     //adds to memory if not blank
-    if (currentText != "") {
-        str = mem.push(currentText);
-        str1 = mem.pushParse(toBeParsed);
-        ui->lineEdit->setText(str);
+    if (currentText != QString("")) {
+        ui->lineEdit->setText("");
+
+        //pops the text into a message box, in the future the string will be sent to be parsed
+        //QMessageBox::information(this, "This will be parsed", ui->lineEdit->text());
+
+        double equalsAnswer = exprtk_parse(toBeParsed);
+        QString buttonText = QString::number(equalsAnswer,'g',15); // 15 is the current double precision
+        ui->label_screen->setText(buttonText);
+
+        string answer;
+        std::stringstream stre;
+        stre << answer << equalsAnswer;
+        answer = stre.str();
+        //push answer into memory
+        str = mem.push(buttonText);
+        answer = mem.pushParse(answer);
     }
-    //pops the text into a message box, in the future the string will be sent to be parsed
-    //QMessageBox::information(this, "This will be parsed", ui->lineEdit->text());
-
-     double equalsAnswer = exprtk_parse(toBeParsed);
-     QString buttonText = QString::number(equalsAnswer,'g',15); // 15 is the current double precision
-     ui->label_screen->setText(buttonText);
-
-     string answer;
-     std::stringstream stre;
-     stre << answer << equalsAnswer;
-     answer = stre.str();
-     //push answer into memory
-     str = mem.push(buttonText);
-     answer = mem.pushParse(answer);
 }
 
 void MainWindow::dividePressed() {
